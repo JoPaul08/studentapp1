91a8d58303dcee27159583cf2ae38fd3c95f0c75
Joseph <danielpaulboglo@gmail.com>
2024-04-03 23:21:23 -0400

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,6 +26,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
